a5727c6d8149a3820e71c8ba35fe17cc2fc0c50f
Cheeseen <aychtang@gmail.com>
2013-09-10 22:15:48 +0100

    Unneeded parens

--- helloworld.elm
+++ helloworld.elm
@@ -4,6 +4,6 @@ draw (w,h) =
   let x = (toFloat w)
       y = (toFloat h)
   in collage 500 500
-        [filled red (rect (x) (y))]
+        [filled red (rect x y)]
 
 main = lift draw Mouse.position
